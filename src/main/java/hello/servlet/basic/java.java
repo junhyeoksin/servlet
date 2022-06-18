6f1d9cb967af72b9d04ed887bc144d080295450b
Jun Hyeok Shin <h-kun@naver.com>
2022-06-18 12:44:55 +0900

    Update: java

--- src/main/java/hello/servlet/basic/java.java
+++ src/main/java/hello/servlet/basic/java.java
@@ -1,20 +1,22 @@
 package hello.servlet.basic;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class java {
     public static void main(String[] args) {
             String [] numbers = new String[]{"1","2","3"};
-            System.out.println("numbers = " + numbers[0]);
-            System.out.println("numbers = " + numbers[1]);
-            System.out.println("numbers = " + numbers[2]);
 
             for(String num : numbers){
                 System.out.println("num =" +  num);
             }
-        Stream<String> num = Arrays.stream(numbers);
-        System.out.println("stream num  = " + num);
-        num.forEach(System.out::println);
+
+            Stream<String> stream = Arrays.stream(numbers);
+            stream.forEach(System.out::println);
+
+
+
+
     }
 }
