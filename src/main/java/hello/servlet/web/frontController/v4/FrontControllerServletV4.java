d145643d63bbe8a36da775278912b22fc8008fd9
Jun Hyeok Shin <h-kun@naver.com>
2022-06-29 23:47:07 +0900

    Update: FrontControllerV4

---
+++ src/main/java/hello/servlet/web/frontController/v4/FrontControllerServletV4.java
@@ -0,0 +1,69 @@
+package hello.servlet.web.frontController.v4;
+
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.MyView;
+import hello.servlet.web.frontController.v3.ControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontController.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontController.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontController.v4.controller.MemberSaveControllerV4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerV1Map  = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        controllerV1Map.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerV1Map.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerV1Map.put("/front-controller/v4/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        System.out.println("request -> " + requestURI);
+
+        ControllerV4 controller = controllerV1Map.get(requestURI);
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+
+        //  개체 컬렉션을 반복하는 데 사용   asIterator    ( option + command + m ) 메서드를 밖으로 뺌
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+
+        String  viewName = controller.process(paramMap, model);// 꺼내온 것을 넣어줌
+
+        MyView view = viewResolver(viewName);
+
+        view.render(model, request, response);
+
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();   // map 생성
+        request.getParameterNames().asIterator()   // 모든 request Parameter name 을 가져옴
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));   // map 에 반복을 하면서 키 값을 꺼내옴
+        return paramMap;
+    }
+}
